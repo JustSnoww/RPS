d763b43faf834cf636b585ccb0f11a562ff8d226
JustSnoww <98555747+JustSnoww@users.noreply.github.com>
2024-11-06 10:56:21 -0600

    fumbling through JS

---
+++ script.js
@@ -0,0 +1,9 @@
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
+
+console.log(getComputerChoice());
+  
+
